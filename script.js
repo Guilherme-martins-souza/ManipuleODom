d3e7c2068118f0205b1e20177fbab24120b967a9
Guilherme <logingui@gmail.com>
2023-02-08 21:54:25 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,44 +1,71 @@
-import botaoDeleta from "./componentes/botãoDeletaTarefa.js";
+import botaoDeleta from "./componentes/botaoDeletaTarefa.js";
 import botaoConcluir from "./componentes/botaoConcluiTarefa.js"
 
-( () => {
-//aponta para o botão do html
-const botao = document.querySelector('[data-botao]');
+// IIFE (Immediately Invoked Function Expression)
+(function () {
+    let listaTarefas = [];
 
-//informa ao navegador que caso seja clicado no botão executura uma função anonima
-botao.addEventListener('click', (evento) =>{
+    // function responsável por visualizar/capturar os inputs do usuário
+    const novaTarefa = (evento) => {
 
-    // faz com que o botão de submit não atualize a pagina
-    evento.preventDefault();
+        // faz com que o botão de submit não atualize a pagina
+        evento.preventDefault();
 
-    // pega o que está escrito no input de texto
-    const inputTexto = document.querySelector('[data-texto]');
+        // esta linha selecionamos a UL do html
+        const listaTarefas = document.querySelector('[data-lista]');
+        //const itemTarefa = document.querySelector('[data-item]')
 
-    // esta linha selecionamos a UL do html
-    const listaTarefas = document.querySelector('[data-lista]');
-    //const itemTarefa = document.querySelector('[data-item]')
+        // pega o que está escrito no input de texto
+        const inputTexto = document.querySelector('[data-texto]');
+        const valor = inputTexto.value;
 
-    // criando um elemento LI
-    const itemLista = document.createElement('li');
-    // adicionando classe tarefas no nosso elemento LI
-    itemLista.classList.add('tarefas');
+        // selecionando o input de data do html
+        const calendario = document.querySelector('[data-form-date]');
+        // utiliza a API moment para formatar o elemante date
+        const dataFormatada = moment(calendario.value).format('L')
 
-    // adicionando um P e --- um BUTTON dentro da classe LI.
-    // infelizmente como nosso buton vai precisar de um eventlistener será necessario
-    // criar uma function que cria esse botao e logo depois adiciona o eventlistener
-    const alteracao = `<p>${inputTexto.value}</p>`;
+        //criando um object 
+        const dados = {
+            valor,
+            dataFormatada
+        }
 
-    // Reescrevendo a LI que criamos pela que escrevemos
-    itemLista.innerHTML = alteracao;
+        const tarefa = criarTarefa(dados)
 
-    // adicionando o botão dentro da LI que criamos
-    itemLista.appendChild(botaoConcluir());
-    itemLista.appendChild(botaoDeleta());
+        //adicionando o elemento LI que criamos dentro da UL.
+        listaTarefas.appendChild(tarefa);
 
-    //adicionando o elemento LI que criamos dentro da UL.
-    listaTarefas.appendChild(itemLista);
+        sessionStorage.setItem('tarefas', dados )
+        inputTexto.value = ' '
 
-    inputTexto.value = ' '
-})
 
-})
+    }
+
+    //function responsável por criar a tarefa
+    const criarTarefa = ({ valor, dataFormatada }) => {
+
+        // criando um elemento LI
+        const itemLista = document.createElement('li');
+
+        // adicionando classe tarefas no nosso elemento LI
+        itemLista.classList.add('tarefas');
+
+        // adicionando um P e dentro da LI.
+        const alteracao = `<p>${valor} * ${dataFormatada}</p>`;
+
+        // Reescrevendo a LI que criamos pela que escrevemos
+        itemLista.innerHTML = alteracao;
+
+        // adicionando o botão dentro da LI que criamos
+        itemLista.appendChild(botaoConcluir());
+        itemLista.appendChild(botaoDeleta());
+
+        return itemLista;
+    }
+
+    //aponta para o botão do html
+    const botao = document.querySelector('[data-botao]');
+    botao.addEventListener('click', novaTarefa);
+})();
+
+    
